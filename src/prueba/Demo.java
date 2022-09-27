b535bee6d8fb4ed862bb5076bcf729b3a47cba3b
Aladino España <Aladino Espaa@DESKTOP-LJRI95S>
2022-09-27 23:52:15 +0200

    nuevo vommit

--- src/prueba/Demo.java
+++ src/prueba/Demo.java
@@ -3,8 +3,9 @@ package prueba;
 public class Demo {
 	// Con push y merge y mesh
 	private void Sysout() {
-		// TODO Auto-generated method stub y esto awfafa wwrwr
+		// TODO Auto-generated method stub y esto aaaa
 		
+		System.out.println();
 		System.out.println();
 		System.out.println();
 
