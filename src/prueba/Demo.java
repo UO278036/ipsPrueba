2f1e20e9fda7e0f85de78a60dafdd0ca9575069c
Aladino España <Aladino Espaa@DESKTOP-LJRI95S>
2022-09-27 23:50:27 +0200

    commit 1

--- src/prueba/Demo.java
+++ src/prueba/Demo.java
@@ -3,7 +3,7 @@ package prueba;
 public class Demo {
 	// Con push y merge y mesh
 	private void Sysout() {
-		// TODO Auto-generated method stub y esto
+		// TODO Auto-generated method stub y esto awfafa wwrwr
 		
 		System.out.println();
 		System.out.println();
